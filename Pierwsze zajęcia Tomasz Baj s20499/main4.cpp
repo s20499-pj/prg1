b6bddabafb43bad0b20e0f42d43533ce6c5f8381
Tomasz Baj <s20499@pjwstk.edu.pl>
2019-11-03 16:04:50 +0100

    Nowy folder

---
+++ Pierwsze zajęcia Tomasz Baj s20499/main4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main()
+{
+int i;
+cout << "Podaj jakąś liczbę";
+cin >> i;
+cout << "Nadałeś zmienną wartość" << i;
+return 0;
+}
