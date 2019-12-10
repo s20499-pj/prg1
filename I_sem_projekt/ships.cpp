4dd8968ff751de2b93b9eae9ca7f79794fd1626b
Tomasz Baj <s20499@pjwstk.edu.pl>
2019-12-10 20:06:49 +0100

    Project v2

--- I_sem_projekt/ships.cpp
+++ I_sem_projekt/ships.cpp
@@ -1,21 +1,73 @@
 #include <iostream>
+using namespace std;
+//using std::cout;
+//using std::cin;
+//using std::endl;
 
-using std::cout;
-using std::cin;
-using std::endl;
+class Board{
+        char board[10][10];
+        public:
+        Board(){
+                for(int m=0; m<10; m++){
+                        for(int n=0; n<10; n++){
+                                board[m][n]='~';
+                        }
+                }
+        }
+        void change(char x, int m, int n){
+                board[m][n]=x;
+        }
+        void print(int m, int n){
+                cout << board[m][n];
+        }
+        void draw(){
+                system("clear");
+                for(int m=0; m<10; m++){
+                        for(int n=0; n<10; n++){
+                                cout << " ";
+                                this->print(m,n);
+                        }cout << endl;
+                }
+        }
+
+        void setship(){
+                int m=5;
+                int n=5;
+                char key=' ';
+                do{
+                        system("clear");
+                        char cpmap[10][10];
+                        for(int i=0; i<100; i++) cpmap[i/10][i%10]=board[i/10][i%10];
+                        cpmap[m][n]='M';
+                        for(int m=0; m<10; m++){
+                                for(int n=0; n<10; n++){
+                                        cout << " " << cpmap[m][n];
+                                }cout << endl;
+                        }
+                        key=getchar();
+                        switch(key){
+                        case 'w':
+                                if(m>0) m--;
+                                break;
+                        case 'a':
+                                if(n>0) n--;
+                                break;
+                        case 's':
+                                if(m<9) m++;
+                                break;
+                        case 'd':
+                                if(n<9) n++;
+                                break;
+                        }
+                }
+                while(key!='q');
+        }
+};
+
 
 int main(){
-	char board[10][10];
-	for(int m=0; m<10; m++){
-		for(int n=0; n<10; n++){
-			board[m][n]='~';
-		}
-	}
-	for(int m=0; m<10; m++){
-		for(int n=0; n<10; n++){
-			cout << ' ' << board[m][n];
-		}
-		cout << endl;
-	}
+	Board board;
+	board.setship();
+	return 0;	
 }
 
